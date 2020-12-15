7861bbeaad487fad4a7bc01572fc046aa2ec11ec
Korben Kirscht <kirscht@gmail.com>
2020-12-14 17:59:34 -0800

    14.2 setup

---
+++ vagrant/packagebox/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo useradd -c 'Vagrant User' -m -d /home/vagrant/ -s /bin/bash \
+  -p '$6$P0FeyUHPZUE$vNjtENRE.ovpgFSXRE16qK0t2c0kKUMIOeabN6j3TihVf2a1n4FwOpDsCwYbTeU2v8e2hjlZsVv325ahJyy070' vagrant
