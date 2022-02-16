4b77c99ac532d64b9b50c5614cd254798a8ba45f
korbenkirscht <korben@kirscht.com>
2022-02-16 14:58:58 -0800

    update deployer

--- vnc/startserver.sh
+++ vnc/startserver.sh
@@ -12,10 +12,13 @@ interact -o -nobuffer -re $prompt return
 expect "${LOGNAME}@localhost's password: "
 send "${LOGNAME}\r"
 expect "from 127.0.0.1\r\r\n"
-# send "touch me\r"
-send "vncserver -localhost no\r"
+send "vncserver -localhost no -depth 32\r"
 expect "to connect to the VNC server."
 sleep 10
 send "exit\r"
 close
 EOF
+
+# https://stackoverflow.com/questions/27569885/how-can-i-redirect-debug-information-to-a-text-file-in-an-expect-script
+# https://askubuntu.com/questions/83824/how-can-i-start-a-vnc-server-before-log-on
+# https://stackoverflow.com/questions/30096462/capture-output-from-ssh-with-expect-script
