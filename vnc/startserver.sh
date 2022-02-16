b388077bd84f33c804766918955e2d89b41a9134
korbenkirscht <korben@kirscht.com>
2022-02-16 14:13:52 -0800

    update deployer

--- vnc/startserver.sh
+++ vnc/startserver.sh
@@ -6,11 +6,11 @@ global spawn_id timeout
 match_max 10000
 set timeout 12
 log_file startserver.log
-spawn ssh vagrant@localhost
+spawn ssh ${LOGNAME}@localhost
 set prompt ":|#|\\\$"
 interact -o -nobuffer -re $prompt return
-expect "vagrant@localhost's password: "
-send "vagrant\r"
+expect "${LOGNAME}@localhost's password: "
+send "${LOGNAME}\r"
 expect "from 127.0.0.1\r\r\n"
 # send "touch me\r"
 send "vncserver -localhost no\r"
